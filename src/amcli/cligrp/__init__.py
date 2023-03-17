89d1d0db0355f4ff7d5d8326ea2c61243a91c9d3
Oliver Nispel <oliver@nispel.org>
2023-03-17 19:08:58 +0100

    colors for alert.state added

--- src/amcli/cligrp/__init__.py
+++ src/amcli/cligrp/__init__.py
@@ -1,4 +1,5 @@
 from datetime import datetime,timezone,tzinfo
+from enum import Enum
 import click,tabulate
 from urllib.parse import unquote
 import amlib.model as model
@@ -16,34 +17,37 @@ DT_FORMATS = [
     '%H:%M'
 ]
 
-state_colors = {
+silence_state_colors = {
     model.State.active: 'green',
     model.State.pending: 'yellow',
     model.State.expired: 'red'
 }
 
 
+alert_state_colors = {
+    model.State1.active: 'green',
+    model.State1.suppressed: 'blue',
+    model.State1.unprocessed: 'red'
+}
+
+
 def echo_silence(silence: model.Silence | model.GettableSilence, tzi: tzinfo | None = timezone.utc) -> None:
     """ Print representation of silence """
     silence_tbl: list[list[str]] = []
-    #if hasattr(silence, 'id'): # only for gettable silences
     if type(silence) == model.GettableSilence:
         silence_tbl.append(['ID', click.style(silence.id, fg='yellow')])
     silence_tbl.append(['Starts at', str(silence.startsAt.astimezone(tzi))])
     silence_tbl.append(['Ends at', str(silence.endsAt.astimezone(tzi))])
-    #if hasattr(silence, 'updatedAt'): # only for gettable silences
     if type(silence) == model.GettableSilence:
         silence_tbl.append(['Updated at', str(silence.updatedAt.astimezone(tzi))])
     silence_tbl.append(['Created by', str(silence.createdBy)])
     silence_tbl.append(['Comment', silence.comment])
-    #if hasattr(silence, 'status'): # only for gettable silences
     if type(silence) == model.GettableSilence:
         silence_tbl.append(['State', click.style(silence.status.state.value,
-               fg=state_colors[silence.status.state])])
+               fg=silence_state_colors[silence.status.state])])
     matchers = [(m.name, matcher_op_to_str(m), m.value)
                 for m in silence.matchers.__root__]
     silence_tbl.append(['Matchers', tabulate.tabulate(matchers, tablefmt='plain')])
-    #if hasattr(silence, 'id'): # only for gettable silences
     if type(silence) == model.GettableSilence:
         silence_tbl.append(['SilenceURL', silence_url(silence)])
     click.echo(tabulate.tabulate(silence_tbl))
@@ -55,8 +59,7 @@ def echo_alert(alert: model.GettableAlert, tzi: tzinfo | None = timezone.utc) ->
     alert_tbl.append(['fingerprint', click.style(alert.fingerprint, fg='yellow')])
     alert_tbl.append(['startsAt', str(alert.startsAt.astimezone(tzi))])
     alert_tbl.append(['endsAt', str(alert.endsAt.astimezone(tzi))])
-    alert_tbl.append(['state', alert.status.state.value])
-    # atb.append(['silencedBy',alert.status.state])
+    alert_tbl.append(['state', click.style(alert.status.state.value, fg=alert_state_colors[alert.status.state])])
     receivers = [['', rec.name] for rec in alert.receivers]
     if len(receivers) > 0:
         receivers[0][0] = 'receivers'
@@ -76,5 +79,6 @@ def echo_alert(alert: model.GettableAlert, tzi: tzinfo | None = timezone.utc) ->
     labels = [[label[0], '=', label[1]] for label in alert.labels]
     if len(labels) > 0:
         alert_tbl.append(['labels', tabulate.tabulate(labels, tablefmt='plain')])
-    alert_tbl.append(['generatorURL', unquote(str(alert.generatorURL))])
+    #alert_tbl.append(['generatorURL', unquote(str(alert.generatorURL))])
+    alert_tbl.append(['generatorURL', str(alert.generatorURL)])
     click.echo(tabulate.tabulate(alert_tbl))

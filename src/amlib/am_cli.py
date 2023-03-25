b754bfde4c9c5d1a8b0135ff2f55472cb1c6860e
onispel <108114863+onispel@users.noreply.github.com>
2023-03-25 12:03:01 +0000

    refactoring

--- src/amlib/am_cli.py
+++ src/amlib/am_cli.py
@@ -11,14 +11,14 @@ from amlib.cligrp.silence import silence_grp
 def main_cli() -> None:
     pass
 
+main_cli.add_command(status_grp)
+main_cli.add_command(silence_grp)
+main_cli.add_command(alert_grp)
 
 if __name__ == '__main__':
 
     conf = read_from_file()
     set_config(conf)
 
-    main_cli.add_command(status_grp)
-    main_cli.add_command(silence_grp)
-    main_cli.add_command(alert_grp)
 
     main_cli()
